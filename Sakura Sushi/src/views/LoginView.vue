dc86cb4cac4fa115f2402005cac4c464adef161a
Amaya <amayaesquivel97@gmail.com>
2026-07-23 12:55:27 -0600

    Creación de proyecto y página de inicio de sesión para usuarios

---
+++ Sakura Sushi/src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login-container">
+    <div class="login-card">
+      <h1>Sakura Sushi</h1>
+      <h2>Iniciar Sesión</h2>
+
+      <form @submit.prevent="login">
+        <input
+          v-model="correo"
+          type="email"
+          placeholder="Correo"
+          required
+        />
+
+        <input
+          v-model="password"
+          type="password"
+          placeholder="Contraseña"
+          required
+        />
+        <a href="NewUserView.vue">Nuevo empleado?</a>
+
+        <button type="submit">
+          Entrar
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import '../assets/css/login.css'
+const correo = ref('')
+const password = ref('')
+
+const login = () => {
+  console.log(correo.value)
+  console.log(password.value)
+
+  // Conectar con firebase para autenticación
+}
+</script>
+
