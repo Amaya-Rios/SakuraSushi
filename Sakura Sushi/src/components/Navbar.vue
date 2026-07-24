475c7d28a13681762ebbbbfc91c9116f3c95d261
Amaya <amayaesquivel97@gmail.com>
2026-07-24 17:34:18 -0600

    Create Navbar.vue
    Creación de componente de barra de navegación con links a interfaz que muestre las mesas del establecimiento, menú de platillos e inicio de sesión

---
+++ Sakura Sushi/src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class= "navbar">
+        <div class="logo">
+            <img src="../assets/image/logo.png" alt="Logo de Sakura Sushi" />
+        </div>
+        <ul class="navegacion">
+            <li><router-link to="/mesas">Mesas</router-link></li>
+            <li><router-link to="/menu">Menú</router-link></li>
+            <li><router-link to="/login">Iniciar Sesión</router-link></li>
+        </ul>
+    </nav>
+</template>
